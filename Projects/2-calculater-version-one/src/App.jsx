4f94e6ae9832de599a86891f5109e39ca63d5b13
dipu123-gupta <kumardipu845420@gmail.com>
2025-12-29 08:50:10 +0530

    file renaming

---
+++ Projects/2-calculater-version-one/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import ButtonContainer from './components/ButtonContainer'
+import Display from './components/Display'
+
+function App() {
+ 
+
+  return (
+    <center className='main-container'>
+     <Display/>
+     <ButtonContainer/>
+    </center>
+  )
+}
+
+export default App
---
+++ Projects/2-calculater-version-one/src/components/Display.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Display = () => {
+  return (
+    <div>
+        <input type="text" className='input' />
+    </div>
+  )
+}
+
+export default Display
