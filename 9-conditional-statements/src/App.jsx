9f5cb1e1180d5454b5c37e4f3e77d3603dd4f1f2
dipu123-gupta <kumardipu845420@gmail.com>
2026-01-02 08:15:46 +0530

    conditional rendering

---
+++ 9-conditional-statements/src/App.jsx
@@ -0,0 +1,52 @@
+import myImage from "./assets/WhatsApp.jpg";
+import "./App.css";
+// import React from "react";
+import { Fragment } from "react";
+
+export const App = () => {
+  return (
+
+    // Fragments allow us to group a list of children without adding extra nodes to the DOM
+    // <> </>  short syntax for <Fragment> </Fragment>
+    // useful when you want to return multiple elements from a component without adding extra nodes to the DOM
+    // especially when rendering lists of components
+    // Example:
+    // [<Component key={1}/>,<Component key={2}/>, ]
+
+    <Fragment>
+      <h1>Hello, React Components!</h1>
+      <Component />
+      <Component />
+
+      <Component />
+      <Component />
+    </Fragment>
+  );
+};
+
+const Component = () => {
+
+  let name = "Dipu Kumar Gupta";
+  let age = 18;
+  let gender = "Male";
+  let address = "Motihari Bihar india";
+  let canWatch="not eligible to watch";
+
+  if(age>=18){
+    canWatch="eligible to watch";
+  }
+
+  return (
+    <div className="component">
+      <img src={myImage} alt="profile" width="300" />
+      <h2>Name:{name} </h2>
+      <h4>age:{age} </h4>
+      <h4>gender: {gender}</h4>
+      <h4>address:{address} </h4>
+
+      {/* Conditional rendering based on age */}
+      {/* <button>{age>=18 ? "Watch now" : "Not eligible to watch"}</button> */}
+      <button>{canWatch}</button>
+    </div>
+  );
+};
