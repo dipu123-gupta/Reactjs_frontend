b54abe15023d0cc945abc429272e1e51eaf9756c
dipu123-gupta <kumardipu845420@gmail.com>
2026-01-03 21:54:40 +0530

    conditional styling in jsx

--- 11-Netflix/src/components/SeriseCard.jsx
+++ 11-Netflix/src/components/SeriseCard.jsx
@@ -3,13 +3,13 @@ const SeriesCard = ({ series }) => {
     <li className="card">
       <img src={series.img_url} alt={series.name} />
       <h2>{series.name}</h2>
-      <p><strong>Rating:</strong> {series.rating}</p>
+      <p><strong>Rating:</strong> <span className={series.rating >= 8.5? "rating" : "rating1"}>{series.rating}</span></p>
       <p>{series.description}</p>
 
       <p><strong>Genre:</strong> {series.genre.join(", ")}</p>
       <p><strong>Cast:</strong> {series.cast.join(", ")}</p>
 
-      <a href={series.watch_url} target="_blank" rel="noopener noreferrer">
+      <a href={series.watch_url} target="_blank" rel="noopener noreferrer" className={series.rating >= 8.5? "rating" : "rating1"}>
         ▶ Watch Now
       </a>
     </li>
